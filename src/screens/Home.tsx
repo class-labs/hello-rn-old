32fc06eed1de53f5f895152582c2be5e254631e2
Simon <sstur@me.com>
2023-02-02 13:22:01 -0800

    Setup for task 7

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,13 +2,20 @@ import { Button, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
-import data from '../data/github.json';
 import { RootStackParamList } from '../types/Navigation';
 
 type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
 
+const orgUsernames = ['microsoft', 'adobe', 'facebook', 'intuit'];
+
+async function getOrgs(orgUsernames: Array<string>) {
+  // TODO: Return an Array<Org>, each Org can be fetched at the following:
+  // https://api.github.com/orgs/${orgUsername}
+}
+
 export function Home() {
   const navigation = useNavigation<NavigationProp>();
+  //TODO: Load the data via the function above, leverage useQuery
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Orgs</Text>
