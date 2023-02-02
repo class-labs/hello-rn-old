b157a61c21b1416679f222a787ba3d4bf632bd4d
Simon <sstur@me.com>
2023-02-02 13:07:03 -0800

    Use react-query

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from './screens/Home';
 import { Details } from './screens/Details';
 import { RootStackParamList } from './types/Navigation';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
+const queryClient = new QueryClient();
+
 export function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Details" component={Details} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
--- src/screens/Details.tsx
+++ src/screens/Details.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { RouteProp } from '@react-navigation/native';
 import { RootStackParamList } from '../types/Navigation';
 import { Repo } from '../types/GithubApi';
+import { useQuery } from '@tanstack/react-query';
 
 type DetailsScreenRouteProp = RouteProp<RootStackParamList, 'Details'>;
 
@@ -19,32 +19,22 @@ async function getOrgDetails(orgId: string): Promise<Array<Repo>> {
 export function Details() {
   const route = useRoute<DetailsScreenRouteProp>();
   const { orgId } = route.params;
-  const [repoList, setRepoList] = useState<Array<Repo> | null>(null);
-  const [error, setError] = useState<Error | null>(null);
+  const { data, isLoading, error } = useQuery(['org-details', orgId], () =>
+    getOrgDetails(orgId),
+  );
 
-  useEffect(() => {
-    getOrgDetails(orgId)
-      .then((repoList) => {
-        setRepoList(repoList);
-      })
-      .catch((error) => {
-        setError(error);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  if (error !== null) {
+  if (error) {
     return <Text>{String(error)}</Text>;
   }
 
-  if (repoList === null) {
+  if (isLoading || data === undefined) {
     return <Text>Loading...</Text>;
   }
 
   return (
     <View style={{ flex: 1, alignItems: 'flex-start', padding: 20 }}>
       <Text>Org: {route.params.orgId}</Text>
-      {repoList.map((repo) => (
+      {data.map((repo) => (
         <Text key={repo.id}>{repo.name}</Text>
       ))}
     </View>
