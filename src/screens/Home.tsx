962a5c9befcb169248e65e000bfec2db3d6db883
Simon <sstur@me.com>
2023-02-02 10:25:59 -0800

    Setup for task5

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,17 @@
 import { Button, Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+import data from '../data/github.json';
 import { RootStackParamList } from '../types/Navigation';
 
 type NavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
 
 export function Home() {
+  // TODO: [Task 5] Display a list of the orgs in the JSON data. If the user
+  // presses one of the items, we will navigate to the details page. For now,
+  // the details page will just display the param that was passed in, nothing
+  // more.
   const navigation = useNavigation<NavigationProp>();
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
