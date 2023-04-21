c1a19feb74b885886f31e870e8a7ba5450cde8e8
Simon <sstur@me.com>
2023-04-20 19:36:20 -0700

    Task 1 solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import { SafeAreaView, View, Text, Button } from 'react-native';
+
 export function App() {
-  let count = 0;
+  const [count, setCount] = useState(0);
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ padding: 20 }}>
         <Text>Count: {count}</Text>
-        <Button title="Click me" />
+        <Button
+          title="Click me"
+          onPress={() => {
+            setCount((count) => count + 1);
+          }}
+        />
       </View>
     </SafeAreaView>
   );
