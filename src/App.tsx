73a83755ecd0721eed8d717bc3253db3f007c57d
Simon <sstur@me.com>
2023-05-08 10:20:23 +0200

    Add TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
