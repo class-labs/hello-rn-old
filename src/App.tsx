5ddac5f8d445320345682db4a3178975b60ed4f2
Simon <sstur@me.com>
2023-05-16 09:11:52 -0700

    Setup task 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+
+const tasks = [
+  { id: 1, name: "Make coffee" },
+  { id: 2, name: "Do laundry" },
+  { id: 3, name: "Learn JavaScript" },
+  { id: 4, name: "Buy milk" },
+  { id: 5, name: "Order lunch" },
+];
 
 export function App() {
   return (
-    <Text style={{ fontSize: 28 }}>
-      Welcome to the fantastic world of React Native!
-    </Text>
+    <View style={{ padding: 30 }}>
+      {/**
+       * Exercise 1
+       * Render a list of tasks from the array above.
+       * You will need to loop the array and render one <Text> for each item.
+       * Note: Remove this comment and replace with your code
+       */}
+    </View>
   );
 }
