8ab1c127e034ec4fcbb9d20f132ff7f4e8526105
Simon <sstur@me.com>
2023-05-15 19:39:57 -0700

    Setup for task2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
 import { SafeAreaView, View, Text, Button } from 'react-native';
-
+type Task = {
+  id: number;
+  name: string;
+};
+const initialTasks: Array<Task> = [
+  { id: 1, name: 'Make coffee' },
+  { id: 2, name: 'Do Laundry' },
+];
 export function App() {
-  const [count, setCount] = useState(0);
+  const [taskItems, setTaskItems] = useState(initialTasks);
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ padding: 20 }}>
-        <Text>Count: {count}</Text>
+        <Text>Tasks:</Text>
+        {/* TODO: loop through the tasks and for each one display task.title  */}
         <Button
-          title="Click me"
+          title="Add new task"
           onPress={() => {
-            setCount((count) => count + 1);
+            // TODO
           }}
         />
       </View>
