511d72d70fefa2480371b401324b72cc9317acf6
Simon <sstur@me.com>
2023-05-16 11:44:02 -0700

    Setup for task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
 import { useState } from "react";
 import { Button, SafeAreaView, Text, View } from "react-native";
 
+const tasks = [
+  { id: 1, name: "Make coffee" },
+  { id: 2, name: "Do laundry" },
+  { id: 3, name: "Learn JavaScript" },
+  { id: 4, name: "Buy milk" },
+  { id: 5, name: "Order lunch" },
+];
+
 export function App() {
-  const [count, setCount] = useState(71);
   return (
     <SafeAreaView>
-      <View style={{ paddingHorizontal: 10 }}>
-        <Text>Count: {count}</Text>
-        <Button
-          onPress={() => {
-            setCount(count + 1);
-          }}
-          title="Increment"
-        />
-        <Button
-          onPress={() => {
-            setCount(count - 1);
-          }}
-          title="Decrement"
-        />
-        <Button
-          onPress={() => {
-            setCount(0);
-          }}
-          title="Reset"
-        />
+      <View style={{ padding: 40 }}>
+        {tasks.map((task) => (
+          <Text key={task.id}>{task.name}</Text>
+        ))}
       </View>
+      {/**
+       * TODO:
+       * Render a button. When the button is clicked, add one item to the list and re-render.
+       * For now, add just a static item, let's say "Buy shoes"
+       */}
     </SafeAreaView>
   );
 }
