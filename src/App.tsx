135a4461a17ff902857541070d0b36134b3311be
Simon <sstur@me.com>
2023-05-18 09:20:14 -0700

    Setup for task 11

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,13 @@ const queryClient = new QueryClient();
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
+// Task 11
+// When the user presses on a movie, we want to navigate to a movie details page
+// Add a Stack.Screen below
+// Remember to add one entry to the RootStackParamList type also
+// Create a file for the MovieDetails screen (put it in the "screens" folder)
+// For now, just render a blank page that says "Movie Details"
+
 export function App() {
   return (
     <QueryClientProvider client={queryClient}>
