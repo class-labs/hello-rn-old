9d7c6d42f9d162e24b27463322782e337244cf0f
Simon <sstur@me.com>
2023-05-18 08:55:53 -0700

    Add react navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { Home } from "./screens/Home";
+import { UserProfile } from "./screens/UserProfile";
+import { RootStackParamList } from "./types/navigation";
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="UserProfile" component={UserProfile} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
