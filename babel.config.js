8c7861022c6233a459350b4e839529c7efcc815d
Simon <sstur@me.com>
2023-07-19 15:50:55 +0700

    Add Prettier

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-process.env.EXPO_ROUTER_APP_ROOT = '../../src/app';
+process.env.EXPO_ROUTER_APP_ROOT = "../../src/app";
 
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel'],
+    presets: ["babel-preset-expo"],
+    plugins: ["expo-router/babel"],
   };
 };
