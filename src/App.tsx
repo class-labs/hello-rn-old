62a05ca4e61d1224a35b63a5120a731a2b4c8f8a
Simon <sstur@me.com>
2023-07-19 16:52:24 +0700

    Add Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -11,8 +11,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
